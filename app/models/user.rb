e09a20d2e655241a446c0bf1c088f9e70ba2bc5c
David Zirkle <zirkledw@hotmail.com>
2020-12-15 14:34:57 -0500

    Add real messages to chatroom.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false },
-                      length: { minimum: 3, maximum: 12 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 12 },
+            uniqueness: { case_sensitive: false }
   has_many :messages
   has_secure_password
 end
