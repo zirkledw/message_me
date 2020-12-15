840d7761b4484d006950af11ca91ce673d19cdad
David Zirkle <zirkledw@hotmail.com>
2020-12-15 13:30:56 -0500

    Add message model and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                       length: { minimum: 3, maximum: 12 }
+  has_many :messages
   has_secure_password
 end
